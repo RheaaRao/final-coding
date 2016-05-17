e39a716384017704c2b72a46c57728c67ae2d4d0
RHEAA RAO <RHEAA.RAO@JOURNALISM.CUNY.EDU>
2016-05-16 20:25:26 -0400

    js changes

---
+++ site.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+	$('#menu li').click(function(){
+		console.log(this);
+		var sectionid = $(this).attr('id');
+		console.log(sectionid);
+
+		var url = "https://api.nytimes.com/svc/topstories/v2/"+ sectionid +".json";
+		url += '?' + $.param({
+  		'api-key': "895d0f9965844bdd93426e59dc2fce01"
+		});
+
+		$.ajax({
+		  url: url,
+		  method: 'GET',
+		}).done(function(data) {
+		  // console.log(data.results);
+
+		  for (var i=0; i<data.results.length; i++){
+          console.log(data.results[i].section);
+		  }
+
+		}).fail(function(err) {
+		  throw err;
+		});
+
+	});
+
+});
